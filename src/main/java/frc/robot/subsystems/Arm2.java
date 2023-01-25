cb31f62bb16ddfedc7d7b4b4348fb0b24c7d9803
erica! <115647009+ejiang2@users.noreply.github.com>
2023-01-25 17:41:08 -0500

    Update Arm2.java

--- src/main/java/frc/robot/subsystems/Arm2.java
+++ src/main/java/frc/robot/subsystems/Arm2.java
@@ -1,18 +1,14 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+// import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+// import com.ctre.phoenix.motorcontrol.NeutralMode;
+// import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX; // import for victorspx
-// import com.revrobotics.CANSparkMaxLowLevel.MotorType; // import for sparkmax
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm2 extends SubsystemBase {
     private final VictorSPX victorspx;
-    // private final CANSparkMaxLowLevel sparkmax;
     private static Arm instance = null;
 
     public Arm2() {
@@ -43,23 +39,4 @@ public class Arm2 extends SubsystemBase {
         // works same as talon: input range on joystick y should map to the speed, where both the speed and xbox controller is in range -1,1
         // can also omit first argument for it to default to ControlMode.PercentOutput i believe
     }
-
-    /* commented out until i find the working import
-    // method for the sparkmax (the linear slide)
-    public void setPercentOutputSparkMax(double percent) {
-        
-        if(Math.abs(percent) < 0.08) {
-            percent = 0;
-        }
-      
-        if(percent == 0) {
-            //sparkmax.stopMotor();    
-        }
-        System.out.println(percent);
-
-        sparkmax.set(percent); 
-        // works same as talon: input range on joystick y should map to the speed, where both the speed and xbox controller is in range -1,1
-        // defaults to ControlMode.PercentOutput
-    }
-    */
 }
